60bbaadf19614dfc97c76c158e4cfc054e9773b8
ahad <ahadg446@gmail.com>
2025-02-12 22:26:37 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+const workoutRoutes = require("./routes/workouts")
+
+app.use(cors());
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use("/api/workouts/", workoutRoutes)
+
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI,)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on port ${port}`);
+    })
+
+  }).catch((err) => {
+    console.log(err.messagge)
+  })
